be84c61d55414c39a0ea986ff58655cc0ff7ba3e
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-01-28 17:41:10 -0600

    added proper login functionality

--- Login.cs
+++ Login.cs
@@ -17,14 +17,19 @@ namespace C969_Appointment_Scheduler
     {
         readonly RegionInfo currentRegion = RegionInfo.CurrentRegion;
         readonly ResourceManager rm = new("C969_Appointment_Scheduler.Resources.Login", Assembly.GetExecutingAssembly());
+        public string _connStr = "Server=127.0.0.1;Port=3306;Database=client_schedule;Uid=sqlUser;Password=Passw0rd!";
+        MySqlDataRepository _repository;
+        private BindingList<User> _users;
+
         public Login()
         {
             // *** Localization testing for Mexican Spanish. 
-            // Thread.CurrentThread.CurrentCulture = new CultureInfo("es-MX");
-            // Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-MX");
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("es-MX");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-MX");
 
             InitializeComponent();
-
+            _repository = new(_connStr);
+            _users = _repository.GetAllUsers();
             UsernameLabel.Text = rm.GetString("Username");
             PasswordLabel.Text = rm.GetString("Password");
             LoginButton.Text = rm.GetString("Login");
@@ -35,19 +40,28 @@ namespace C969_Appointment_Scheduler
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            if (UsernameBox.Text.Equals("test") && PasswordBox.Text == "test")
+            bool validLogin = false;
+            // Foreach user in db
+            foreach(User user in _users)
             {
-                string userName = UsernameBox.Text;
-                CalendarView cv = new("Server=127.0.0.1;Port=3306;Database=client_schedule;Uid=sqlUser;Password=Passw0rd!", userName);
-                cv.Show();
-                // Open Login_History.txt, create if not existing with RW permissions
-                using (StreamWriter w = File.AppendText("Login_History.txt"))
-                    w.WriteLine($"{DateTime.UtcNow} - {userName}");
-                this.Hide();
+                // if the user's username = usernamebox && the user's password == passwordbox, login.
+                if (user.UserName == UsernameBox.Text && user.Password == PasswordBox.Text)
+                {
+                    validLogin = true;
+                    string userName = UsernameBox.Text;
+                    CalendarView cv = new(_connStr, userName);
+                    cv.Show();
+                    // Open Login_History.txt, create if not existing with RW permissions
+                    using (StreamWriter w = File.AppendText("Login_History.txt"))
+                        w.WriteLine($"{DateTime.UtcNow} - {userName}");
+                    this.Hide();
+                    return;
+                }
             }
-            else
+
+            if (validLogin == false)
             {
-                MessageBox.Show(rm.GetString("LoginFailed"));
+                MessageBox.Show("Login information is incorrect.");
             }
         }
     }
